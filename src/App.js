bdb479be755af0d9973208de9c081fad0b25da4b
fuadmiazi <fuad.hasan.1997@gmail.com>
2022-05-15 16:07:34 +0600

    Displaying product component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Product from './components/Product/Product';
 import Shop from './components/Shop/Shop';
 
 
@@ -9,6 +10,7 @@ function App() {
     <div >
      <Header></Header>
      <Shop></Shop>
+     
 
     </div>
   );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import './Shop.css';
+import Product from '../Product/Product';
 
 const Shop = () => {
-    console.log(fakeData);
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    
+
     return (
-        <div>
-            <h1>This is shop</h1>
+        <div className="shop-container">
+            <div className="product-container">
+                
+            {
+                products.map(pd => <Product product={pd}></Product>)
+            }
+               
+            </div>
+           
+           <div className="cart-container">
+               <h3>This is cart</h3>
+           </div>
+            
         </div>
     );
 };
