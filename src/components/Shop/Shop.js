1cf81ab75307a344c7fe820d4deed5ecbdc018f9
fuadmiazi <fuad.hasan.1997@gmail.com>
2022-05-23 22:42:46 +0600

    Cart Information

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
@@ -9,7 +10,6 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
     
     const handleAddProduct = (product) =>{
-        console.log('Product Added', product);
         const newCart = [...cart, product];
         setCart(newCart);
     }
@@ -29,8 +29,7 @@ const Shop = () => {
             </div>
            
            <div className="cart-container">
-               <h3>This is cart</h3>
-               <h5>Order Summary: {cart.length}</h5>
+               <Cart cart={cart}></Cart>
            </div>
             
         </div>
